7bac51605a307795f2fbb875ab394420cdd0e01a
pilz0 <git@ketamin.trade>
2025-02-12 09:42:37 +0100

    yolo

--- setup.sh
+++
@@ -1,5 +0,0 @@
-!# /bin/bash
-ansible-galaxy role install Oefenweb.fail2ban
-ansible-galaxy role install hifis.unattended_upgrades
-ansible-galaxy role install geerlingguy.swap
-ansible-galaxy collection install devsec.hardening
